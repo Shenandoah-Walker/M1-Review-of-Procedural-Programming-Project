b0721a214ad1cdc74f4f0ae66b1091e5b39e94bb
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-09-21 20:55:29 +0000

    Restarted Gradebook program

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -5,82 +5,23 @@
 
 using namespace std;
 
+const int maxTestScores = 50;
+
 /*
-Function: getNames
-Purpose: A function to read the data from the file. This function should be provided with a file object
-for a file that has already been opened successfully. The function should read the data from
-the file into the names and test scores parallel arrays. The function should also return the
-number of student records stored on the file.
-Parameters: ifstream&, string[], int[], int
-Returns: int
-Preconditions: The file must be opened successfully.
-Postconditions: The names and test scores arrays will be filled with data from the file.
+Function: getTestData
+Purpose: To read the data from the file 
+Parameters: 
+ - studentName: an array of strings that will hold the names of the students
+ - testScores: a 2D array of integers that will hold the test scores of the students
+ - numStudentsRecords: an integer that will hold the number of student records read from the file
+ Returns: The number of student records read from the file
+ Preconditions: The file must already be open and validated.
+ Postconditions: The students' names will be stored in the studentName array and the test scores will be stored in the testScores array. 
 */
 
-int getNames(ifstream&, string[], int[], int[]);
+int getTestData(string studentName[], int testScores[][maxTestScores], int numStudentsRecords);
 
 /*
-Function: calculateAverage
-Purpose: A function to calculate the averages of all the students. This function should be provided
-with the test scores array and return the averages in the averages array.
-Parameters: int[][], int[]
-Returns: void
-Preconditions: The test scores array must be filled with data.
-Postconditions: The averages array will be filled with the averages of the students.
-*/
-
-void calculateAverage(int[][], int[]);
-
-/*
-Function: calculateLetterGrade
-Purpose: A function to calculate a letter grade from an average test score. This function is provided
-with the test score and returns the letter grade.
-Parameters: int
-Returns: char
-Preconditions: The test score must be a valid test score.
-Postconditions: The letter grade will be returned.
-*/
-
-char calculateLetterGrade(int);
-
-/*
-Function: createReport
-Purpose: A function to produce the formatted report. This function is provided with the student
-  names array and the averages array. It then outputs the students name, average test score
-  and letter grade. The report should have labeled columns and the data should be aligned in
-  columns.
-Parameters: string[], int[]
-Returns: void
-Preconditions: The names and averages arrays must be filled with data.
-Postconditions: The report will be displayed on the screen.
-  */
-
-int main() {
-
-    string fileName;
-  //Create a file object to use for file input.
-    ifstream inFile;
-  
-  //Create a string array to hold the names of the students. The array should be able to hold 50 names.
-    string names[50];
-  //Create a 2D array with a row for each student and a column for each test score. The array should be able to hold 50 students and 10 test scores.
-    int testScores[50][10];
-  //Create an array to hold the average test scores for each student. The array should be able to hold 50 averages.
-    int averages[50];
-
-    //Ask the user which file they would like to open.
-    cout << "Enter the name of the file you would like to open: ";
-    cin >> fileName;
-    //Open the file.
-    inFile.open(fileName);
-    //If the file failed to open, close the file and display an error message. Then, allow the user to enter another file name.
-    while (inFile.fail()) {
-        inFile.close();
-        cout << "Error opening file. Please enter another file name: ";
-        cin >> fileName;
-        inFile.open(fileName);
-    }
-      
-  
-  
-}
+Function: calcAverage
+Purpose: To calculate the average test score for each student. The test scores array is passed by reference to this function, and the averages are returned in a new array of doubles called averages.
+Parameters: 
--- main.cpp
+++ main.cpp
@@ -1,86 +1,71 @@
-//Rock, Paper, Scissors Program
+//Gradebook program
 
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
+const int maxTestScores = 50;
+
 /*
-Function: getNames
-Purpose: A function to read the data from the file. This function should be provided with a file object
-for a file that has already been opened successfully. The function should read the data from
-the file into the names and test scores parallel arrays. The function should also return the
-number of student records stored on the file.
-Parameters: ifstream&, string[], int[], int
-Returns: int
-Preconditions: The file must be opened successfully.
-Postconditions: The names and test scores arrays will be filled with data from the file.
+Function: getTestData
+Purpose: To read the data from the file 
+Parameters: 
+ - inputFile: a reference to an ifstream object that will be used to read the data from the file
+ - studentName: an array of strings that will hold the names of the students
+ - testScores: a 2D array of integers that will hold the test scores of the students
+ - numStudentsRecords: an integer that will hold the number of student records read from the file
+ Returns: The number of student records read from the file
+ Preconditions: The file must already be open and validated.
+ Postconditions: The students' names will be stored in the studentName array and the test scores will be stored in the testScores array. 
 */
 
-int getNames(ifstream&, string[], int[], int[]);
+int getTestData(ifstream &inputFile, string studentName[], int testScores[][maxTestScores], int numStudentsRecords);
 
 /*
-Function: calculateAverage
-Purpose: A function to calculate the averages of all the students. This function should be provided
-with the test scores array and return the averages in the averages array.
-Parameters: int[][], int[]
-Returns: void
-Preconditions: The test scores array must be filled with data.
-Postconditions: The averages array will be filled with the averages of the students.
+Function: calcAverage
+Purpose: To calculate the average test score for each student. The test scores array is passed by reference to this function, and the averages are returned in a new array of doubles called averages.
+Parameters: 
+  - testScores: a 2D array of integers that holds the test scores of the students
+  - numStudentsRecords: an integer that holds the number of student records read from the file
+  - averages: an array of doubles that will hold the average test scores of the students
+Returns: The averages array of doubles
+Preconditions: The testScores array must already hold the test scores of the students.
+Postconditions: The averages array will hold the average test score for each student.
 */
 
-void calculateAverage(int[][], int[]);
+ void calcAverage(int testScores[][maxTestScores], int numStudentsRecords, double averages[]);
 
 /*
-Function: calculateLetterGrade
-Purpose: A function to calculate a letter grade from an average test score. This function is provided
+Function: calcLetterGrade
+Purpose: Calculates a letter grade from an average test score. This function is provided
 with the test score and returns the letter grade.
-Parameters: int
-Returns: char
-Preconditions: The test score must be a valid test score.
-Postconditions: The letter grade will be returned.
+Parameters:
+ - average: a double that holds the average test score of a student
+Returns: The letter grade as a char
+Preconditions: The average must be a valid double.
+Postconditions: The letter grade will be returned as a char.
 */
 
-char calculateLetterGrade(int);
+char calcLetterGrade(double average);
 
 /*
 Function: createReport
-Purpose: A function to produce the formatted report. This function is provided with the student
-  names array and the averages array. It then outputs the students name, average test score
-  and letter grade. The report should have labeled columns and the data should be aligned in
-  columns.
-Parameters: string[], int[]
-Returns: void
-Preconditions: The names and averages arrays must be filled with data.
-Postconditions: The report will be displayed on the screen.
-  */
+Purpose: To produce the formatted report. This function is provided with the student
+names array and the averages array. It then outputs the students name, average test score
+and letter grade. The report should have labeled columns and the data should be aligned in
+columns.
+Parameters:
+ - studentName: an array of strings that holds the names of the students
+ - averages: an array of doubles that holds the average test scores of the students
+ - numStudentsRecords: an integer that holds the number of student records read from the file
+ - letterGrade: a char that holds the letter grade of a student
+Returns: None
+Preconditions: The studentName and averages arrays must already hold the names and average test scores of the students.
+Postconditions: The report will be printed to the console.
+*/
+
+void createReport(string studentName[], double averages[], int numStudentsRecords);
 
 int main() {
-
-    string fileName;
-  //Create a file object to use for file input.
-    ifstream inFile;
-  
-  //Create a string array to hold the names of the students. The array should be able to hold 50 names.
-    string names[50];
-  //Create a 2D array with a row for each student and a column for each test score. The array should be able to hold 50 students and 10 test scores.
-    int testScores[50][10];
-  //Create an array to hold the average test scores for each student. The array should be able to hold 50 averages.
-    int averages[50];
-
-    //Ask the user which file they would like to open.
-    cout << "Enter the name of the file you would like to open: ";
-    cin >> fileName;
-    //Open the file.
-    inFile.open(fileName);
-    //If the file failed to open, close the file and display an error message. Then, allow the user to enter another file name.
-    while (inFile.fail()) {
-        inFile.close();
-        cout << "Error opening file. Please enter another file name: ";
-        cin >> fileName;
-        inFile.open(fileName);
-    }
-      
-  
-  
 }
